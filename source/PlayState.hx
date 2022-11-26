8cda35e3e6252343aa3fab15bfea0fd97ea293b1
Mini Raposa Gamer <115371844+MiniRaposaGAMER@users.noreply.github.com>
2022-11-26 12:58:59 -0300

    pt br seus burro

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -83,16 +83,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['bolacha cristal', 0.2], //From 0% to 19%
+		['Merda', 0.4], //From 20% to 39%
+		['Burro', 0.5], //From 40% to 49%
+		['vc é medio', 0.6], //From 50% to 59%
+		['geração z', 0.69], //From 60% to 68%
+		['Nao sei', 0.7], //69%
+		['parece eu', 0.8], //From 70% to 79%
+		['Eminem', 0.9], //From 80% to 89%
+		['BfReiDelas@gmail.com', 1], //From 90% to 99%
+		['BfReiDelas@gmail.com', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
@@ -2301,9 +2301,9 @@ class PlayState extends MusicBeatState
 
 	public function updateScore(miss:Bool = false)
 	{
-		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
-		+ ' | Rating: ' + ratingName
+		scoreTxt.text = 'Pontuação: ' + songScore
+		+ ' | Erros: ' + songMisses
+		+ ' | Classificação: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
 		if(ClientPrefs.scoreZoom && !miss && !cpuControlled)
